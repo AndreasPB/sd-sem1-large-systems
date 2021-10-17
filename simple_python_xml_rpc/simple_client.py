5044ed75c42b62aa4a02fd7591f8009f1dd42044
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2021-10-17 18:51:52 +0200

    Add environment variable support + rich

--- simple_python_xml_rpc/simple_client.py
+++ simple_python_xml_rpc/simple_client.py
@@ -1,9 +1,20 @@
-import xmlrpc.client
 import datetime
+import os
+import xmlrpc.client
+from time import sleep
 
-proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
+from dotenv import load_dotenv
+from rich import print
 
-today = proxy.today()
-# convert the ISO8601 string to a datetime object
-converted = datetime.datetime.strptime(today.value, "%Y%m%dT%H:%M:%S")
-print("Today: %s" % converted.strftime("%d.%m.%Y, %H:%M"))
+
+load_dotenv()
+
+HOST_ADDRESS = os.getenv("HOST_ADDRESS")
+proxy = xmlrpc.client.ServerProxy(f"http://{HOST_ADDRESS}:1234/")
+
+while True:
+    today = proxy.today()
+    # convert the ISO8601 string to a datetime object
+    converted = datetime.datetime.strptime(today.value, "%Y%m%dT%H:%M:%S")
+    print("Today: %s" % converted.strftime("%d.%m.%Y, %H:%M:%S"))
+    sleep(1)
--- simple_python_xml_rpc/simple_server.py
+++ simple_python_xml_rpc/simple_server.py
@@ -1,12 +1,20 @@
 import datetime
-from xmlrpc.server import SimpleXMLRPCServer
+import os
 import xmlrpc.client
+from xmlrpc.server import SimpleXMLRPCServer
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def today():
     today = datetime.datetime.today()
     return xmlrpc.client.DateTime(today)
 
-server = SimpleXMLRPCServer(("localhost", 8000))
-print("Listening on port 8000...")
+HOST_ADDRESS = os.getenv("HOST_ADDRESS")
+print(HOST_ADDRESS)
+server = SimpleXMLRPCServer((f"{HOST_ADDRESS}", 1234))
+print("Listening on port 1234...")
 server.register_function(today, "today")
 server.serve_forever()
